b9490319343e8b9de80746dca280fec13d35c3cd
kevin.fang <keigi1203@gmail.com>
2020-01-12 09:00:17 +0000

    Refine before release

--- novel/wsgi.py
+++ novel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ck101toMobi project.
+WSGI config for novel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ck101toMobi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "novel.settings")
 
 application = get_wsgi_application()
